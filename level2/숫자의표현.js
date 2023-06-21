34057f9d8e88a3731b96bd9d4f38445d04de7811
kiseokDev <dev.leekiseok@gmail.com>
2023-06-21 19:46:59 +0900

    숫자의 표현 완료

--- level2/숫자의표현.js
+++ level2/숫자의표현.js
@@ -1,4 +1,27 @@
 function solution(n) {
-    var answer = 0;
-    return answer;
+    let count = 1;
+    let start = 1;
+    let sum = 0;
+    let end = Math.ceil(n / 2); // 연속된 합의 최대 값
+
+    while (start <= end && n != 1) {
+        for (let i = start; sum < n; i++) {
+            // console.log(`sum:${sum}`)
+            sum += i;
+            if (sum === n) {
+                count++;
+                break;
+            } else if (sum > n) {
+                break;
+            }
+        }
+        sum = 0;
+        start++;
+    }
+
+    return count;
+}
+
+for (let n = 1; n <= 20; n++) {
+    console.log(solution(n))
 }
